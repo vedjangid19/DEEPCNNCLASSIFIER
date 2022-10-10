4712715f95b53bb8f55bfcc942340190c30d1328
vedjangid19 <vedprakashjangid.jpr@gmail.com>
2022-10-11 00:34:53 +0530

    updated the constant

--- src/deepClassifier/constants/__init__.py
+++ src/deepClassifier/constants/__init__.py
@@ -1,4 +1,4 @@
-from pathlib import Path
+from pathlib import Path 
 
-CONFIG_FILE_PATH = Path("configs/config.yaml")
-PARAMS_FILE_PATH = Path("params.yaml")
+CONFIG_FILE_PATH = Path("configs/config.yaml") 
+PARAMS_FILE_PATH = Path("params.yaml") 
