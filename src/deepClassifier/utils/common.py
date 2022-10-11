7aa8129e1b921f95e7a7e1a9b332a31be51d7cc7
vedjangid19 <vedprakashjangid.jpr@gmail.com>
2022-10-12 01:25:34 +0530

    common util added.

--- src/deepClassifier/utils/common.py
+++ src/deepClassifier/utils/common.py
@@ -1,7 +1,7 @@
 import os
 from box.exceptions import BoxValueError
 import yaml
-from deepClassifier import logger
+from src.deepClassifier import logger
 import json
 import joblib
 from ensure import ensure_annotations
