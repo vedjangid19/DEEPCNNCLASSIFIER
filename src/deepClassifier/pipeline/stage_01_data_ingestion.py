3d495f74355dc5334487b7b54999c6e5f46bab1d
vedjangid19 <vedprakashjangid.jpr@gmail.com>
2022-10-14 02:26:07 +0530

    DI Pipeline added

--- src/deepClassifier/pipeline/stage_01_data_ingestion.py
+++ src/deepClassifier/pipeline/stage_01_data_ingestion.py
@@ -4,7 +4,7 @@ from src.deepClassifier import logger
 
 STAGE_NAME = "Data Ingestion stage"
 
-def main():
+def main(): 
     config = ConfigurationManager()
     data_ingestion_config = config.get_data_ingestion_config()
     data_ingestion = DataIngestion(config=data_ingestion_config)
