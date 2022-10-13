362d9c558c21617ad0775b88275802224ecea445
vedjangid19 <vedprakashjangid.jpr@gmail.com>
2022-10-13 23:48:07 +0530

    st-01 trail run success

---
+++ src/deepClassifier/pipeline/stage_01_data_ingestion.py
@@ -0,0 +1,21 @@
+from src.deepClassifier.config import ConfigurationManager
+from src.deepClassifier.components import DataIngestion
+from src.deepClassifier import logger
+
+STAGE_NAME = "Data Ingestion stage"
+
+def main():
+    config = ConfigurationManager()
+    data_ingestion_config = config.get_data_ingestion_config()
+    data_ingestion = DataIngestion(config=data_ingestion_config)
+    data_ingestion.download_file()
+    data_ingestion.unzip_and_clean()
+
+if __name__ == '__main__':
+    try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
